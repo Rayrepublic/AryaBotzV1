ff52845746803a150e1efdbe92981cfb29e462ec
RayNoElitz± <145004377+Rayrepublic@users.noreply.github.com>
2023-09-17 19:19:38 +0700

    Update main-donate.js

--- plugins/main-donate.js
+++ plugins/main-donate.js
@@ -3,14 +3,11 @@ let handler = async(m, { conn, usedPrefix, command }) => {
 
     let don = `
 ≡ *DONACION*
-puedes donar si quieres ayudar a mantener el bot activo
-
-▢ *PayPal*
-• *Link :* https://paypal.me/fg98f
-
-▢ *Mercado Pago Arg*
-• *Alias :* fg98ff
-• *N° :* 1168352204
+Berapapun donasi kalian akan sangat berarti buat saya :) 
+❏ DANA
+0831-9934-6086
+❏ GOPAY
+0831-9934-6086
 `
 let img = 'https://i.ibb.co/37FP2bk/donate.jpg'
 conn.sendFile(m.chat, img, 'img.jpg', don, m)
@@ -18,6 +15,6 @@ conn.sendFile(m.chat, img, 'img.jpg', don, m)
 }
 handler.help = ['donate']
 handler.tags = ['main']
-handler.command = ['apoyar', 'donate', 'donar'] 
+handler.command = ['donasi', 'donate', 'donar'] 
 
 export default handler
